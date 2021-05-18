0d83d2d35be6b2a50bbbecb2132090e90bd731e2
david.burkhart <david.burkhart@hftm.ch>
2021-05-18 18:26:28 +0200

    d) add useful links with examples

--- src/main/java/piano/Piano.java
+++ src/main/java/piano/Piano.java
@@ -1,5 +1,11 @@
 package piano;
 
+/**
+ * java sound: https://docs.oracle.com/javase/tutorial/sound/index.html
+ * sequencers: https://docs.oracle.com/javase/tutorial/sound/MIDI-seq-intro.html
+ * stackoverflow simple piano: https://codereview.stackexchange.com/questions/58439/very-basic-java-piano
+ * staclpverflow violin tuner: https://codereview.stackexchange.com/questions/57502/using-sounds-in-java/57516#57516
+ */
 public class Piano {
 	public static void main(String[] args) {
 		PianoApp.main(args);

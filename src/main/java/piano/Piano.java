21cf45bd8fb33742c724400bb193f24f8e0414ef
David Burkhart <dburkhart@previon.ch>
2021-05-18 17:37:37 +0200

    b) initial java fx project setup (example)

---
+++ src/main/java/piano/Piano.java
@@ -0,0 +1,7 @@
+package piano;
+
+public class Piano {
+	public static void main(String[] args) {
+		PianoApp.main(args);
+	}
+}
---
+++ src/main/java/piano/PianoApp.java
@@ -0,0 +1,37 @@
+package piano;
+
+import java.io.IOException;
+import javafx.application.Application;
+import javafx.fxml.FXMLLoader;
+import javafx.scene.Scene;
+import javafx.stage.Stage;
+
+public class PianoApp extends Application {
+
+    private Stage primaryStage;
+
+    public static void main(String[] args) {
+        launch(args);
+    }
+
+    @Override
+    public void start(Stage primaryStage) {
+        this.primaryStage = primaryStage;
+        this.primaryStage.setTitle("JavaFX App");
+        this.showMainView();
+    }
+
+    public void showMainView() {
+        try {
+            FXMLLoader loader = new FXMLLoader();
+            loader.setLocation(PianoApp.class.getResource("/fxml/MainView.fxml"));
+
+            // Show the scene containing the root layout.
+            Scene scene = new Scene(loader.load());
+            primaryStage.setScene(scene);
+            primaryStage.show();
+        } catch (IOException e) {
+            e.printStackTrace();
+        }
+    }
+}
